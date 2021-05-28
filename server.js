671f708d269a140d0f1c22d02769b624de8ed5dd
krunalshastri00 <krunalshastri00@gmail.com>
2021-05-28 13:52:00 +0530

    Added route for transaction

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const dotEnv = require('dotenv').config();
 const mongoose = require('mongoose');
+const Transaction = require('./Models/transactionModel');
 
 const app = express();
+app.use(express.json());
 
 mongoose.connect(process.env.ATLAS_URL, {
   useNewUrlParser: true,
@@ -13,6 +15,20 @@ mongoose.connect(process.env.ATLAS_URL, {
 
 app.get('/', (req, res) => {
   res.send('Hello Darlings!');
+});
+
+app.post('/', async (req, res) => {
+  console.log(req.body);
+  const { text, amount } = req.body;
+
+  try {
+    const newTrans = new Transaction({ text, amount });
+    await newTrans.save();
+    res.json('Transaction Added!');
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server error!');
+  }
 });
 
 const PORT = process.env.PORT || 5000;
