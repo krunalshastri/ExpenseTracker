f912ef03323c226a426eb55c26d8a92a1ce1fcc1
krunalshastri00 <krunalshastri00@gmail.com>
2021-05-30 16:54:31 +0530

    Proceeding for deploy

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const dotEnv = require('dotenv').config();
 const mongoose = require('mongoose');
 const cors = require('cors');
+const path = require('path');
 
 const app = express();
 app.use(express.json());
@@ -15,6 +16,14 @@ mongoose.connect(process.env.ATLAS_URL, {
   useCreateIndex: true,
   useFindAndModify: false,
 });
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
